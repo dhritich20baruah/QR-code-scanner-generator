7a5a0bfce958dbdc6e708ecfbdf3bf54ce66c2ce
Dhriti <dhriti.baruah20@gmail.com>
2026-04-16 21:57:23 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TouchableOpacity } from 'react-native';
+import QRCodeScanner from './components/QRCodeScanner';
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <QRCodeScanner/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   );
 }
 
---
+++ components/QRCodeScanner.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { Camera, CameraView } from "expo-camera";
+import { useNavigation } from "@react-navigation/native";
+import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const initializeDB = async (db) => {
+    try {
+        await db.execAsync(`
+          PRAGMA journal_mode = WAL;
+          CREATE TABLE IF NOT EXISTS qrHistory (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, date TEXT NOT NULL, qrData TEXT NOT NULL);     
+          `);
+        console.log("DB connected");
+    } catch (error) {
+        console.log("Error in connecting DB", error);
+    }
+};
+
+export default function QRCodeScanner() {
+    return (
+        <SQLiteProvider databaseName="QRApp.db" onInit={initializeDB}>
+            <Scanner />
+        </SQLiteProvider>
+    )
+}
+
+export function Scanner() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [torch, setTorch] = useState(false);
+    const navigation = useNavigation();
+    const db = useSQLiteContext();
+
+    useEffect(() => {
+        const getCameraPermissions = async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === "granted");
+        };
+
+        getCameraPermissions();
+    }, []);
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    const toggleTorch = () => {
+        setTorch((current) => (current === false ? true : false));
+    };
+
+    const handleScanResult = async ({ data }) => {
+        setScanned(true);
+        let dateObj = new Date()
+        let dateString = dateObj.toISOString();
+        let date = dateString
+            .slice(0, dateString.indexOf("T"))
+            .split("-")
+            .reverse()
+            .join("-");
+
+        // Extract and format the time (hh:mm)
+        let time = dateObj.toTimeString().slice(0, 5); // Extracts HH:MM
+
+        // Combine date and time
+        let dateTime = `${date} ${time}`;
+
+        await db.runAsync("INSERT INTO qrHistory (date, qrData) values (?, ?)", [
+            dateTime,
+            data,
+        ]);
+        await navigation.navigate("ScanResult", { qrData: data });
+    };
+
+    return (
+        <View style={StyleSheet.container}>
+            <CameraView
+                onBarcodeScanned={scanned ? undefined : handleScanResult}
+                barcodeScannerSettings={{
+                    barcodeTypes: ["qr", "pdf417"],
+                }}
+                style={StyleSheet.absoluteFillObject}
+                enableTorch={torch}
+            />
+            {scanned && (
+                <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+            )}
+            <View style={{ flex: 1, margin: 50 }}>
+                <TouchableOpacity style={styles.button} onPress={toggleTorch}>
+                    <Text>
+                        {torch === false ? (
+                            <Ionicons
+                                name="flashlight-outline"
+                                size={30}
+                                color="white"
+                            />
+                        ) : (
+                            <Ionicons
+                                name="flash-off-outline"
+                                size={30}
+                                color="white"
+                                style={styles.btnText}
+                            />
+                        )}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+    },
+    button: {
+        flex: 1,
+        alignSelf: "flex-end",
+        alignItems: "center",
+    },
+});
